599f90c715888a74a96ce089bd3b3aadb1c15650
tahlly88@gmail.com <64144102+tahlly88@users.noreply.github.com>
2022-04-05 19:55:58 -0400

    init upload

--- app/src/main/java/com/zybooks/lightsoutnav/MainActivity.java
+++ app/src/main/java/com/zybooks/lightsoutnav/MainActivity.java
@@ -1,14 +1,36 @@
 package com.zybooks.lightsoutnav;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.zybooks.lightsoutnav.R;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
+                    R.id.navigation_game, R.id.navigation_color, R.id.navigation_help)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+            NavigationUI.setupWithNavController(navView, navController);
+        }
+        navView.setOnNavigationItemReselectedListener(item -> { });
     }
 }
